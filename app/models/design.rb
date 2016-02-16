5e5f3e6eb80e3744121e8ecfe4f45a84f1f2ba45
rbburkey <robinsonbburkey@gmail.com>
2016-02-15 21:22:18 -0500

    sync images/css

--- app/models/design.rb
+++ app/models/design.rb
@@ -1,4 +1,6 @@
 class Design < ActiveRecord::Base
   has_many :reviews
   belongs_to :company
+  has_attached_file :image, styles: { large: "600x600>"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
